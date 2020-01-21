63a7ccdfae3cf42bab1e20c8299631a1b0703d2b
Julio Villalobos <julio.villalobos@softtek.com>
2020-01-21 08:14:28 -0600

    Rockola

---
+++ Rockola2/Rockola2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace Rockola2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SearchList(string keyword)
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCDfku4wmVrb39gjc3pDt3T9M1OyCYGlwQ",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = keyword;
+            searchListRequest.MaxResults = 10;
+
+            var searchListResponse = searchListRequest.Execute();
+
+            return PartialView(searchListResponse.Items);
+        }
+
+
+        List<string> ListIds = new List<string>();
+        public ActionResult PlayList(string Id)
+        {
+            ListIds.Add(Id);
+            return PartialView(ListIds);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
